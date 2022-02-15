01838d23e0439eea1450493c92d2f808e6170fe3
Leigh Cressman <4342190+LeighCressman@users.noreply.github.com>
2022-02-15 11:26:22 -0500

    First commit

---
+++ redcap_api.R
@@ -0,0 +1,36 @@
+#####################
+# load libraries
+#####################
+
+library(tidyverse)
+library(httr)
+
+
+##################################
+# load REDCap API credentials 
+# (stored locally, out of repo)
+##################################
+
+creds <- read_csv("../redcap_credentials.csv")
+
+
+#####################
+# import REDCap data
+#####################
+
+url <- 'https://redcap.med.upenn.edu/api/'
+formData <- list('token' = creds$token[1],
+                 content = 'report',
+                 format = 'csv',
+                 report_id = creds$report_id[1],
+                 csvDelimiter = '',
+                 rawOrLabel = 'raw',
+                 rawOrLabelHeaders = 'raw',
+                 exportCheckboxLabel = 'false',
+                 returnFormat = 'csv'
+)
+response <- POST(url, body = formData, encode = 'form')
+rc <- type_convert(content(response, type = 'text/csv', 
+                           col_types = cols(.default = col_character()))) %>%
+  as_tibble()
+
